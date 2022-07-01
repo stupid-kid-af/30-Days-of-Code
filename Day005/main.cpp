d1df89122e34a83ba4c3cd85fa883740ac2506b1
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-07-02 00:00:08 +0530

    added fourth-fifth day

---
+++ Day005/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+int no;
+cin >> no;
+for (int n=1; n<=10;n++){
+  cout << no <<" x " << n << " = " << no*n << endl;
+ }
+return 0;
+}
